d31e89ace14c345f3c1ca65bd1b0804fe62346d3
Carlos Mejia <carlos.selectronics@gmail.com>
2016-09-16 23:11:55 -0600

    Nueva Actualizacion

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Portal de Consulta de Medicamentos</title>
 	<html lang="es">
-	<meta charset="utf-8" />
+	<meta charset= "utf-8" />
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 	<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,14 +11,24 @@
   <body>
 	  
 	  <header>
-             <img src="../img/bannerportal.png" alt="logo" width="1023" height="141" />
+             <img src="../img/bannerportal.png" alt="logo" width="1023" height="141" position: center/>
                  
       </header>
-  <h1>HOLA MUNDO EN PHP</h1>
+  <h1 style="text-align:center">INGRSE LOS DATOS DE SU BUSQUEDA</h1>
 
+
+
+<form name="ejemplo" action="22-html5-search-input.php" method="POST">
+	Escriba el medicamento a buscar: <input type="search" name="busqueda"><br>
+	<input type="submit" value="Enviar">
+</form>
+ 
+ 
+ 
   <footer>
     	
   </footer>
   </body>
 </html>  
+
 
