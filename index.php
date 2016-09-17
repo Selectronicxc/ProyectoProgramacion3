47a79a02b4b7c681d8f6ea34051ca13926ac1c77
Carlos Mejia <carlos.selectronics@gmail.com>
2016-09-16 21:59:41 -0600

    Actualizado

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 
 <!DOCTYPE html>
 <html>
- <title>Portal de Consulta de Medicamentos</title>
 <head>
+	<title>Portal de Consulta de Medicamentos</title>
+	<html lang="es">
+	<meta charset="utf-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
   <body>
+	  
+	  <header>
+             <img src="../img/bannerportal.png" alt="logo" width="1023" height="141" />
+                 
+      </header>
+  <h1>HOLA MUNDO EN PHP</h1>
 
   <footer>
     	
